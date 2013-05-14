19b5ecfec7a509522a45c191ba2b825369d1e01b
Rob Wierzbowski <hello@robwierzbowski.com>
2013-05-14 17:17:01 -0400

    Revise config files

--- app/templates/conditional/sass/config.rb
+++ app/templates/conditional/sass/config.rb
@@ -1,24 +1,15 @@
-# Require any additional compass plugins here
+# If you're using global Compass extensions require them here
 # example require 'extension'
 
 # All preprocessors output expanded code
-# Assets will be minified and revved on build by Grunt
+# Grunt handles minification and revs assets on build
 environment = :development
 output_style = :expanded
 asset_cache_buster :none
 
-# RWRW Should these dirs point to the .tmp or should they point to the jekyll app? How does contrib-compass work?
-# Contrib Compass usually runs from gruntfile location.
-# RWRW Question is shouldyo be able to pull jekyll site out of yeo and have it work with no alterations, or should the yeo site be clean?
-## If it compiles direct, will css min etc still get to it?
-
-# Directory settings, relative to config.rb (RWRW or GRUNTFILE?)
+# Directory settings, relative to config.rb
+# Grunt handles sass, css, js, and image source and destination paths
 http_path = "/" # Set to the root of your project when deployed
-sass_dir = "<%= cssPreDir %>"
-css_dir = "_site/<%= cssDir %>"
-images_dir = "<%= imgDir %>"
-generated_images_dir = "_site/<%= imgDir %>/sprites"
-javascripts_dir = "_site/%= jsDir %>"
 extensions_dir = "components"
 
 # Enable relative paths for Compass asset helpers
